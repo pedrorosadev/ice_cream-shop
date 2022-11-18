36686b0fe70ad82bda99d0a11c41d5ac51377000
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-11-18 11:57:27 -0300

    Aplicando JS no site

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleChangeText() {
+    let text = document.querySelector('#titlePage');
+
+    text.innerHTML = "Promação de 25% OFF em Todos os Produtos";
+    text.style.color = "red";
+}
+
+function handleChangeTextOut() {
+    let text = document.querySelector('#titlePage');
+
+    text.innerHTML = "Produtos";
+    text.style.color = "black";
+}
